a80ca9b0a5d89d320891aa3036e6c27ee2c22e6a
islem <contact@islem-fortas.site>
2025-07-14 10:58:39 +0100

    rest of assignments in js file

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
-console.log(
-  " %cElzero %cWeb %cSchool",
-  "color:red ;font-size: 40px",
-  "color:green ;font-size: 40px ; font-weight: bold",
-  "background-color:blue ; forground-color:white ;font-size: 40px"
-);
+// Assignment 03
+// console.log(
+//   " %cElzero %cWeb %cSchool",
+//   "color:red ;font-size: 40px",
+//   "color:green ;font-size: 40px ; font-weight: bold",
+//   "background-color:blue ; forground-color:white ;font-size: 40px"
+// );
+
+// Assignment 04 
+
+// console.log("Assignment3");
+// console.group("Group 1");
+// console.log("Message One");
+// console.log("Message Two");
+// console.group("Child Group");
+// console.log("Message One");
+// console.log("Message Two");
+// console.group("Grand Child Group");
+// console.log("Message One");
+// console.log("Message Two");
+// console.groupEnd();
+// console.groupEnd();
+// console.groupEnd();
+// console.group("Group 2");
+// console.log("Message One");
+// console.log("Message Two");
+// console.groupEnd();
+
+// Assignment 05
+// console.table(["Elzero", "Ahmed", "Sameh", "Gamal", "Aya"]);
+
+// Assignment 06
+// console.log("Iam In Console");
+// document.write("Iam In Page");
